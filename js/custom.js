6b49ac8bfd917f90aff3a75b92e923a49a2b7d19
ismizo <nerdyjinnyas@gmail.com>
2022-07-24 09:18:49 +0300

    addedtitleanime

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,26 @@
-
 (async () => {
     
       const anime = await import("/githome/js/titleanimation.js");
+      //const theme = await import("/githome/js/themes.js");
+      //const server = await import("/githome/js/server.js");
     
     return anime;
   })().then((anime)=>{
     const animate = new anime.titleanimation();
+    console.log(animate);
     animate.start();
+    //animate.doTitleAnimation();
   });
+
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    console.log(newColorScheme)
+});
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log("dark");
+}else{
+    console.log("light");
+}
--- js/titleanimation.js
+++ js/titleanimation.js
@@ -1,28 +1,59 @@
 export {titleanimation as titleanimation};
+
 class titleanimation {
     
     constructor(){
         this.start = this.start;
         this.counter1 = 0;
-        this.counter2 = {"zero":[],"one":[]}
+        this.interval1 = this.interval1;
+        this.counter2 = {"zero":["u","dhu","i"],"one":["di","gi","ta","li"],"num":0};
+        this.title1 =  document.getElementsByClassName("title1")[0];
+        this.doTitleAnimation = this.doTitleAnimation;
+    
         
     }
+
+    interval1 = 696;
 
     
 
     start () {
-        const title = document.querySelector(".title1");
-        const interval1 = window.setInterval(()=>{
-            if(this.counter1==0){
-                title.style.color="red";
-                this.counter1++;
-            }else{
-                title.style.color="black";
-                this.counter1--;
-            }
-            console.log(this.counter1)
+        
+        setInterval(()=>{this.doTitleAnimation(this.interval1,this.counter1)},this.interval1);
+        
+    }
 
-        },969)
+    doTitleAnimation (){
+        const span = document.createElement("span");
+        const span2 = document.createElement("span");
+       
+        if(this.counter1==0&&this.counter2.num>=0){
+            if(this.counter2.num>2){
+
+            }else{
+                span.innerHTML = this.counter2.zero[this.counter2.num];
+                document.getElementById("ma").appendChild(span);
+            }
+            span2.innerHTML = this.counter2.one[this.counter2.num];
+            document.getElementById("ki").appendChild(span2);   
+            if(this.counter2.num>=3){
+                this.counter1 = 1;
+            }else{
+                this.counter2.num++;
+            }
+             
+        }else{
+            if(this.counter2.num>=1){
+                document.getElementById("ma").querySelectorAll("span")[this.counter2.num].remove();
+            }
+            document.getElementById("ki").querySelectorAll("span")[this.counter2.num+1].remove();
+            if(this.counter2.num==0){
+                this.counter1 = 0;
+            }else{
+                this.counter2.num--;
+            }
+
+        }
     }
 
 }
