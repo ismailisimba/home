b3aea8c3f4bfc1cae48a54fe18d731ddcd8b9f9d
ismizo <nerdyjinnyas@gmail.com>
2022-07-25 11:57:28 +0300

    loppp

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,15 @@
-/*(async () => {
+(async () => {
     
-      //const anime = await import("/githome/js/titleanimation.js");
-      //const theme = await import("/githome/js/themes.js");
+      const anime = await import("/githome/js/titleanimation.js");
+      const theme = await import("/githome/js/themes.js");
       //const server = await import("/githome/js/server.js");
     
-    //return {anime,theme};
+    return {anime,theme};
   })().then(({anime,theme})=>{
-    //const themes = new theme.themes();
-    //const animate = new anime.titleanimation();
-    //animate.start();
+    const themes = new theme.themes();
+    const animate = new anime.titleanimation();
+    animate.start();
     //console.log(themes.currentTheme);
-    //themes.addThemes();
+    themes.addThemes();
   });
-*/
+
